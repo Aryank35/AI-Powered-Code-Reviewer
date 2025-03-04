55a2d55d60b33c452c7003aec5245f43f275370a
AryanEVA <aryanmohapatra@evawinoptimize.com>
2025-03-04 10:32:57 +0530

    Gemini Ai integrated

--- BackEnd/src/services/ai.service.js
+++ BackEnd/src/services/ai.service.js
@@ -1,9 +1,18 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash",
+    systemInstruction: `
+    You are an code reviewer, who have an experties in development.
+    You look for the code and find the problems and suggest the solution to the developer.
+    `
+ });
 
-const prompt = "Explain how AI works";
+async function generateContent(prompt) {
+    if (!prompt) throw new Error("Prompt is required");
 
-const result = await model.generateContent(prompt);
-console.log(result.response.text());
+    const result = await model.generateContent(prompt);
+    return result.response.text();
+}
+
+module.exports = generateContent;
