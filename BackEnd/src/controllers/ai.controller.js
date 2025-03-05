e2228fb78e0fe68cc68b206e70514ed4476c71f8
AryanEVA <aryanmohapatra@evawinoptimize.com>
2025-03-05 15:04:37 +0530

    all files uploaded

--- BackEnd/src/controllers/ai.controller.js
+++ BackEnd/src/controllers/ai.controller.js
@@ -1,17 +1,17 @@
-const aiService = require("../services/ai.service");
+const aiService = require("../services/ai.service")
+
 
 module.exports.getReview = async (req, res) => {
-    try {
-        const prompt = req.body.prompt;
 
-        if (!prompt) {
-            return res.status(400).json({ error: "Prompt is required" });
-        }
+    const code = req.body.code;
 
-        const response = await aiService(prompt);
-        res.json({ response });
-    } catch (error) {
-        console.error("AI Service Error:", error);
-        res.status(500).json({ error: "Internal Server Error" });
+    if (!code) {
+        return res.status(400).send("Prompt is required");
     }
-};
+
+    const response = await aiService(code);
+
+
+    res.send(response);
+
+}
